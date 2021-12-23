505f3d03283a5d8801d5e46557e2f4d76c6fd4fa
Clément <clement.mercier@hotmail.com>
2021-12-23 13:26:08 +0100

    ♻️ Update project structure

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO Utilisateur (numero_passeport, date_de_naissance, nom, numero_telephone, pays, prenom, secret)
-VALUES ('76TE87654', '1997-02-01', 'Mercier', '+33649235682', 'France', 'Clément',
+VALUES ('76TE87654', '1997-01-02', 'Mercier', '+33649235682', 'France', 'Clément',
         '$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6');
